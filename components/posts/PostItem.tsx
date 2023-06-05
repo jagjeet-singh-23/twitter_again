bd9b8f84a37e57551324bb38615086329af1e593
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-06-05 13:34:10 +0530

    modified:   components/posts/PostFeed.tsx 	modified:   components/posts/PostItem.tsx 	new file:   hooks/useFollow.ts 	new file:   pages/api/follow.ts 	modified:   pages/users/[userId].tsx

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from "next/router";
 import { useCallback, useMemo } from "react";
 import { formatDistanceToNowStrict } from "date-fns";
+import { AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useLoginModal from "@/hooks/useLoginModal";
+
+import Avatar from "../Avatar";
 
 interface PostItemProps {
     data: Record<string, any>;
@@ -48,7 +51,77 @@ const PostItem: React.FC<PostItemProps> = ({
                 cursor-pointer 
                 transition
             ">
-
+            <div className="flex flex-row items-start gap-3">
+                <Avatar userId={data.user.id} />
+                <div>
+                    <div className="flex flex-row items-center gap-2">
+                        <p
+                            onClick={goToUser}
+                            className="
+                                text-white 
+                                font-semibold 
+                                hover:underline 
+                                cursor-pointer
+                            ">
+                            {data.user.name}
+                        </p>
+                        <span
+                            onClick={goToUser}
+                            className="
+                                text-neutral-500 
+                                cursor-pointer
+                                hover:underline
+                                hidden
+                                md:block
+                            ">
+                            @{data.user.username}
+                        </span>
+                        <span
+                            className="
+                                text-neutral-500
+                                text-sm
+                            ">
+                            {createdAt} ago
+                        </span>
+                    </div>
+                    <div className="text-white mt-1">
+                        {data.body}
+                    </div>
+                    <div className="flex flex-row items-center mt-3 gap-10">
+                        <div
+                            className="
+                            flex 
+                            flex-row 
+                            items-center 
+                            text-neutral-500 
+                            gap-2 
+                            cursor-pointer 
+                            transition 
+                            hover:text-sky-500
+                            "
+                        >
+                            <AiOutlineMessage size={20} />
+                            <p>{data.comments?.length || 0}</p>
+                        </div>
+                        <div
+                            onClick={onLike}
+                            className="
+                            flex 
+                            flex-row 
+                            items-center 
+                            text-neutral-500 
+                            gap-2 
+                            cursor-pointer 
+                            transition 
+                            hover:text-red-500
+                            "
+                        >
+                            <AiOutlineHeart size={20} />
+                            <p>{data.comments?.length || 0}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
