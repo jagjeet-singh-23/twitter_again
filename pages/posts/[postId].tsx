c5d25ca39c70c8b4e97971dec447b3fe3ead1d1c
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-06-06 20:04:34 +0530

    new file:   components/posts/CommentFeed.tsx 	new file:   components/posts/CommentItem.tsx 	modified:   pages/api/posts/[postId].ts 	modified:   pages/posts/[postId].tsx

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -4,7 +4,7 @@ import usePost from "@/hooks/usePost";
 import Header from "@/components/Header";
 import PostItem from "@/components/posts/PostItem";
 import Form from "@/components/Form";
-
+import CommentFeed from "@/components/posts/CommentFeed";
 
 const PostView = () => {
     const router = useRouter();
@@ -21,7 +21,12 @@ const PostView = () => {
         <>
             <Header label="Tweet" showBackArrow />
             <PostItem data={fetchedPost} />
-            <Form placeholder="Tweet your reply"/>
+            <Form
+                postId={postId as string}
+                isComment
+                placeholder="Tweet your reply"
+            />
+            <CommentFeed comments={fetchedPost?.comments} />
         </>
     )
 }
